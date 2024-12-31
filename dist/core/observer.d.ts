568eb7107f7e41c8807790d719a43bf0359ddcf4
cartia <luca.rondanini@gmail.com>
2024-12-31 09:25:43 -0800

    progress

--- dist/core/observer.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Observer<T extends Record<string, (...args: any[]) => void>> {
-    private eventMap;
-    constructor();
-    emit<K extends keyof T>(event: K, ...args: Parameters<T[K]>): void;
-    on<K extends keyof T>(event: K, callback: T[K]): void;
-    off<K extends keyof T>(event: K, callback: T[K]): void;
-}
