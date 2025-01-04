a87491860fd184424ccc2726db097fde383de320
cartia <luca.rondanini@gmail.com>
2025-01-03 22:06:02 -0800

    progress

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import SwayJs from "./core/swayjs";
-
-export * from "./core/swayjs";
-export * from "./core/context";
-export * from "./core/route";
-export * from "./core/exceptions";
-export * from "./core/observer";
-
-export default SwayJs;
